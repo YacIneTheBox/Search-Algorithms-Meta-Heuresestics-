baa5b442ad81ce8516acdac26309a28280268873
YacIneTheBox <111428692+YacIneTheBox@users.noreply.github.com>
2025-11-06 20:28:41 +0100

    implementing random search

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import csv
+import random
+import math
+import copy
 
 class City:
     def __init__(self,name,lat,lon,x,y):
@@ -14,4 +17,33 @@ with open('algeria_20_cities_xy.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
     for row in reader:
-        print (row)
+        
+        name,lat,lon,x,y = row
+        city = City(name,lat,lon,x,y)
+        cities.append(city)
+        
+for c in cities:
+    print(f"{c.name}: ({c.x},{c.y})")
+    
+    
+def Random_search(cities,iteration):
+    nbrCities = cities.size()
+    sCity = "Algiers"
+    totalDistance = 0
+    shortestDistance = float('inf')
+    clonCities = copy.deepcopy(cities)
+         
+    for l in range(iteration):
+        for j in range(nbrCities):
+            random.shuffle(clonCities)
+            index = next(i for i, city in enumerate(clonCities) if city.name == sCity)
+            totalDistance += math.dist([clonCities[index].x,clonCities[index].y],[clonCities[j].x,clonCities[j].y])
+        
+        if totalDistance < shortestDistance:
+            shortestDistance = totalDistance
+            
+        totalDistance = 0
+    return shortestDistance
+
+iteration = 10
+shortestDist = Random_search(cities,iteration)
